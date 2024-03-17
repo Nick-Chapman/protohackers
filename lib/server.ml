3433e11f16ae610471a1187872fb44a3f6f42b5d
Nick Chapman <nic@amadido.co.uk>
2024-03-17 23:14:15 +0000

    fixed number of worker/threads

--- lib/server.ml
+++ lib/server.ml
@@ -8,27 +8,46 @@ let panic s = raise (Panic s)
 let port = 6516
 let addr = inet_addr_any (* NOT: loopback *)
 
-let suck : int -> file_descr -> unit =
+let suck : string -> file_descr -> unit =
   fun who fd ->
   let size = 10000 in
   let buf = Bytes.create size in
   let rec loop() =
     let n = read fd ~buf ~pos:0 ~len:size in
+    (*printf "[%s] Read: %s%!" who (Bytes.sub_string buf 0 n);*)
     if n == 0 then () else (
       let nn = write fd ~buf ~pos:0 ~len:n in
-      (*printf "[%d] Echoed #%d bytes\n%!" who nn;*)
+      (*printf "[%s] Echoed #%d bytes\n%!" who nn;*)
       if (n!=nn) then panic (sprintf "write: %d != %d" n nn);
       loop()
     )
   in
   loop();
   close fd;
-  printf "[%d] Closed\n%!" who;
+  printf "[%s] Closed\n%!" who;
   ()
 
 let string_of_sockaddr = function
   | ADDR_UNIX _ -> "ADDR_UNIX"
   | ADDR_INET (ia,p) -> sprintf "%s:%d" (string_of_inet_addr ia) p
+
+let worker fd name =
+  printf "[%s] Worker Started\n%!" name;
+  let rec loop i =
+    let who = sprintf "%s.%d" name i in
+    let (fd1,addr1) = accept fd in
+    printf "[%s] Accepted: %s\n%!" who (string_of_sockaddr addr1);
+    suck who fd1;
+    loop (i+1)
+  in
+  loop 0
+
+let rec spawn_workers f = function
+  | [] -> ()
+  | [last] -> f last
+  | w::more ->
+     let _t = Thread.create f w in
+     spawn_workers f more
 
 let serve() =
   let () = printf "Serve: %s:%d\n%!" (string_of_inet_addr addr) port in
@@ -39,10 +58,5 @@ let serve() =
   let addr = ADDR_INET (addr,port) in
   let () = bind fd ~addr in
   let () = listen fd ~max:0 in
-  let rec loop who =
-    let (fd1,addr1) = accept fd in
-    printf "[%d] Accepted: %s\n%!" who (string_of_sockaddr addr1);
-    let _t = Thread.create (fun () -> suck who fd1) () in
-    loop (who+1)
-  in
-  loop 1
+  spawn_workers (worker fd) ["a";"b";"c";"d";"e"];
+  ()
