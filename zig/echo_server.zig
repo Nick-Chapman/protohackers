f1fbcee49a99a58a1475a6e143eb76a17a9b7a97
Nick Chapman <nic@amadido.co.uk>
2025-06-15 15:01:22 +0100

    zig echo server: use threads to support concurrent connections

--- zig/echo_server.zig
+++ zig/echo_server.zig
@@ -10,23 +10,26 @@ pub fn main() !void {
     const loopback : net.Ip4Address = try net.Ip4Address.parse("0.0.0.0",portnum);
     const me : net.Address = net.Address{.in = loopback};
     var server : net.Server = try me.listen(.{.reuse_address = true});
-    // TODO: support concurrent connections!
     print("listening on: {any}\n",.{server.listen_address});
     while (true) {
         print("waiting to accept connection...\n",.{});
         const conn : net.Server.Connection = try server.accept();
-        const addr = conn.address;
-        print("{any}: connection accepted\n",.{addr});
-        const stream = conn.stream;
-        const writer = stream.writer();
-        while(true) {
-            var buf : [buffer_size]u8 = undefined;
-            const n : usize = try stream.read(&buf);
-            if (n == 0) break;
-            print("{any}: received {d} bytes\n",.{addr,n});
-            writer.writeAll(buf[0..n]) catch {};
-        }
-        stream.close();
-        print("{any}: connection closed\n",.{addr});
+        _ = try std.Thread.spawn(.{}, handle, .{conn});
     }
+}
+
+fn handle(conn: net.Server.Connection) void {
+    const addr = conn.address;
+    print("{any}: connection accepted\n",.{addr});
+    const stream = conn.stream;
+    const writer = stream.writer();
+    while(true) {
+        var buf : [buffer_size]u8 = undefined;
+        const n : usize = stream.read(&buf) catch 0;
+        if (n == 0) break;
+        print("{any}: received {d} bytes\n",.{addr,n});
+        writer.writeAll(buf[0..n]) catch {};
+    }
+    stream.close();
+    print("{any}: connection closed\n",.{addr});
 }
